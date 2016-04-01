675fa7205c3ff9b58df640f1bd4dbb5167bf0a76
Sasha Go <golubev@fastmail.uk>
2016-04-02 00:22:04 +0700

    store Store.js copy

--- Store.js
+++ Store.js
@@ -8,33 +8,33 @@
 
 class <%= __pascalName__ %> {
 
-	/**
-	 * Creates a new instance of the "__pascalName__" store.
-	 * @param {ServiceLocator} locator The service locator for resolving dependencies.
-	 */
-	constructor(locator) {
+  /**
+   * Creates a new instance of the "<%= __pascalName__ %>" store.
+   * @param {ServiceLocator} locator The service locator for resolving dependencies.
+   */
+  constructor(locator) {
 
-		// In case you have the UHR plugin registered
-		// this._uhr = locator.resolve('uhr');
+  // In case you have the UHR plugin registered
+  // this._uhr = locator.resolve('uhr');
 
-		/**
-		* Current lifetime for data (in milliseconds) which the store is responsible for.
-		* @type {number} Lifetime in milliseconds.
-		*/
-		this.$lifetime = 60000;
-	}
+  /**
+   * Current lifetime for data (in milliseconds) which the store is responsible for.
+   * @type {number} Lifetime in milliseconds.
+   */
+  this.$lifetime = 60000;
+}
 
-	load() {
-		// Here you can do any HTTP requests using this._uhr
-		// or another universal HTTP request implementation.
-		// Please read details here https://github.com/catberry/catberry-uhr.
-	}
+  load() {
+  // Here you can do any HTTP requests using this._uhr
+  // or another universal HTTP request implementation.
+  // Please read details here https://github.com/catberry/catberry-uhr.
+}
 
-	handleSomeAction() {
-		// Here you can call this.$context.changed() if you know
-		// that remote data has been changed.
-		// Also, you can have other actions and handle methods like this, just define a method.
-	}
+  handleSomeAction() {
+  // Here you can call this.$context.changed() if you know
+  // that remote data has been changed.
+  // Also, you can have other actions and handle methods like this, just define a method.
+}
 }
 
 module.exports = <%= __pascalName__ %>;
